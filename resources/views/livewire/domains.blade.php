c8ae40c95ec08589207bd3057387e2198467c814
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2020-08-14 15:17:01 -0500

    Updated Search Placeholder

--- resources/views/livewire/domains.blade.php
+++ resources/views/livewire/domains.blade.php
@@ -10,7 +10,7 @@
             </select>
         </div>
         <div class="flex justify-end">
-            <input class="appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" wire:model="search" placeholder="Search Domains...">
+            <input class="appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" wire:model="search" placeholder="Search...">
         </div>
     </div>
 

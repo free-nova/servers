022ee7451a0a366159e319d69336882cf12a9fb1
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2020-08-18 10:14:57 -0500

    Reduced top padding of domain list.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-header/>
     <div class="flex flex-col min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-        <div class="w-2/3 mt-24 mx-auto">
+        <div class="w-2/3 mx-auto">
             <livewire:domains />
         </div>
     </div>
